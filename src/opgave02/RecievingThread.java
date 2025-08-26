8555219a545fdfabffa158b015f03a147fca8074
lokemoneyb <lokemtg@gmail.com>
2025-08-26 11:06:57 +0200

    Lavet server-svar

---
+++ src/opgave02/RecievingThread.java
@@ -0,0 +1,33 @@
+package opgave02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class RecievingThread extends Thread {
+    Socket connSocket;
+    String message;
+
+    public RecievingThread(Socket connSocket) {
+        this.connSocket = connSocket;
+    }
+
+
+    public void run() {
+        try {
+            BufferedReader inFromAfsender = new BufferedReader(new InputStreamReader(connSocket.getInputStream()));
+            System.out.println("Forbundet til server.");
+
+            while (true) {
+                message = inFromAfsender.readLine();
+                System.out.println("From sender: " + message);
+            }
+
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
--- src/opgave02/TCPClient.java
+++ src/opgave02/TCPClient.java
@@ -1,29 +1,14 @@
 package opgave02;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
-
 
 public class TCPClient {
 
-	public static void main(String[] args) throws Exception, IOException {
+	public static void main(String argv[]) throws Exception {
 
-			String sentence;
-			String modifiedSentence;
-			BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-			Socket clientSocket = new Socket("localhost", 6789);
-			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		Socket clientSocket = new Socket("localhost", 6789);
 
-		while(true) {
-			System.out.println("Indtast et ord");
-			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			sentence = inFromUser.readLine();
-			outToServer.writeBytes(sentence + '\n');
-			modifiedSentence = inFromServer.readLine();
-			System.out.println("FROM SERVER: " + modifiedSentence);
-		}
+		(new TCPSendThread(clientSocket)).start();
+		(new RecievingThread(clientSocket)).start();
 	}
 }
