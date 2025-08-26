697f9702bbba48a6b5d1bf85f8c5c6f4cc426c3e
DIDER <Simon.dider@gmail.com>
2025-08-26 11:24:37 +0200

    Opgave03 opdatering af NavneServerThread

--- src/opgave03/Threads/NavneServerThread.java
+++ src/opgave03/Threads/NavneServerThread.java
@@ -1,4 +1,32 @@
 package opgave03.Threads;
 
-public class NavneServerThread {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class NavneServerThread extends Thread{
+
+    Socket connectionSocket;
+    String message;
+
+    public NavneServerThread(Socket clientSocket) {
+        this.connectionSocket = clientSocket;
+    }
+
+    public void run(){
+        try{
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+            DataOutputStream outToOtherUser = new DataOutputStream(connectionSocket.getOutputStream());
+            System.out.println("Chat løs: ");
+            while(true){
+                message = inFromUser.readLine();
+                outToOtherUser.writeBytes(message + '\n');
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
