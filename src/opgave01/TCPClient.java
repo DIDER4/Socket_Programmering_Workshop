c005b9902accf7d5be48c04a4ea847d6a1f009c9
lokemoneyb <lokemtg@gmail.com>
2025-08-26 09:43:19 +0200

    Lavet server-svar

--- src/opgave01/TCPClient.java
+++ src/opgave01/TCPClient.java
@@ -10,22 +10,20 @@ import java.net.Socket;
 public class TCPClient {
 
 	public static void main(String[] args) throws Exception, IOException {
-		
-		String sentence;
-		String modifiedSentence;
-		
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-		
-		Socket clientSocket = new Socket("localhost", 6789);
-		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-		System.out.println("Indtast et ord");
 
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		sentence = inFromUser.readLine();
-		outToServer.writeBytes(sentence + '\n');
-		modifiedSentence = inFromServer.readLine();
-		System.out.println("FROM SERVER: " + modifiedSentence);
-		
-		clientSocket.close();
+			String sentence;
+			String modifiedSentence;
+			BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+			Socket clientSocket = new Socket("localhost", 6789);
+			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+		while(true) {
+			System.out.println("Indtast et ord");
+			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			sentence = inFromUser.readLine();
+			outToServer.writeBytes(sentence + '\n');
+			modifiedSentence = inFromServer.readLine();
+			System.out.println("FROM SERVER: " + modifiedSentence);
+		}
 	}
 }
