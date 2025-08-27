8a8c43143da18755f5cb27211325467dda096502
DIDER <simon.dider@gmail.com>
2025-08-27 14:04:54 +0200

    Oprettelse af UDPeksempel og template i form af navneserver, server og client

---
+++ src/opgave03/udpEksempel/register/NavneRegister.java
@@ -0,0 +1,22 @@
+package opgave03.udpEksempel.register;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NavneRegister {
+    private Map<String, String> serverAdresser = new HashMap<>();
+
+    public synchronized void registerClient(String nickname, String netInfo) {
+        serverAdresser.put(nickname, netInfo);
+    }
+
+    public synchronized void removeClient(String nickname) {
+        serverAdresser.remove(nickname);
+    }
+
+    public synchronized Collection<Map.Entry<String, String>> getClients() {
+        return serverAdresser.entrySet();
+    }
+}
+
